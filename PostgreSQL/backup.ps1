eb0abc6c2e8e874205244e57afb4d1f02454a378
Razmik <razmik.minasyann@gmail.com>
2022-03-24 15:06:47 +0400

    added function

--- PostgreSQL/backup.ps1
+++ PostgreSQL/backup.ps1
@@ -9,3 +9,19 @@ $dbs = @(
 
 )
 $destination = New-Item -Path $rootPath -ItemType Directory -Name 'backup'
+
+function BackupPGserver {
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$pgserver,
+        
+        [Parameter(Mandatory = $true)]
+        [string]$userName,
+        
+        [Parameter(Mandatory = $true)]
+        [array[]]$dbs,
+        
+        [Parameter(Mandatory = $true)]
+        [string[]]$destination
+    )
