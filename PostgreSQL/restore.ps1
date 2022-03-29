60677656e24ee13df98e552acd3f8a6b69c314dd
Razmik <razmik.minasyann@gmail.com>
2022-03-29 11:28:02 +0400

    added function

--- PostgreSQL/restore.ps1
+++ PostgreSQL/restore.ps1
@@ -4,3 +4,18 @@ $destination = ''
 
 $pgserver = ''
 $userName = ''
+
+function RestorePGserver {
+    Param
+    (
+
+        [Parameter(Mandatory = $true)]
+        [string]$pgserver,
+
+        [Parameter(Mandatory = $true)]
+        [string]$destination,
+
+        [Parameter(Mandatory = $true)]
+        [string]$userName
+
+    )
