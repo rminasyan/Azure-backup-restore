835d137d58a2b5f14771eb660803e3bd0b52f794
Razmik <razmik.minasyann@gmail.com>
2022-04-03 14:44:31 +0400

    added foreach

--- Blob/backup.ps1
+++ Blob/backup.ps1
@@ -9,3 +9,8 @@ $collections = @(
 'add names'
 
 )
+
+foreach($collection in $collections)
+    {
+     azcopy copy "https://$storageAccount.blob.core.windows.net/$collection/$SASToken" $rootPath --recursive=true
+    }
